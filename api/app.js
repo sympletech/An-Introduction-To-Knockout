69d78823aa8e1d651141329f565e425268645b7a
Daniel Lewis <daniel_lewis@esri.com>
2014-11-07 17:35:49 -0800

    added node project

---
+++ api/app.js
@@ -0,0 +1,41 @@
+var restify = require('restify'),
+    _ = require('lodash');
+
+var server = restify.createServer();
+
+server.get('/', function(req, res, next){
+    res.send('An API');
+});
+
+
+
+var cars = [
+    { id: _.uniqueId(), car: "Ford Mustang", speed: 160 },
+    { id: _.uniqueId(), car: "Chevy Camaro", speed: 220 },
+    { id: _.uniqueId(), car: "Dodge Dart", speed: 140 },
+    { id: _.uniqueId(), car: "Acura Integra", speed: 195 },
+    { id: _.uniqueId(), car: "VW Love Bus", speed: 45 }
+];
+
+server.get('/cars', function(req, res){
+    res.send(cars);
+});
+
+server.post('/cars', function(req, res){
+    var itemToSave = JSON.parse(req.params.payload);
+    itemToSave.id = _.uniqueId();
+    cars.push(itemToSave);
+
+});
+
+server.del('/cars/:id', function(req, res){
+    cars = _.remove(cars, function(car){
+        return car.id == req.params.id;
+    });
+});
+
+
+
+server.listen(5150, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
